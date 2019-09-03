4ad748d8a196e6ce7db6b1bfa7d7fb55aeb097dd
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2019-09-03 16:59:40 +0700

    d

--- app/src/main/java/com/gzeinnumer/classandobject_kt/MainActivity.kt
+++ app/src/main/java/com/gzeinnumer/classandobject_kt/MainActivity.kt
@@ -8,5 +8,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val l1 = Lampu()
+        val l2 = Lampu() // Class class = new Class()
+        l1.turnOn()
+        l2.turnOff()
+        l1.displayLightStatus("l1")
+        l2.displayLightStatus("l2")
+        l1.setStatusLamp(false)
+        l2.setStatusLamp(true)
+        l1.displayLightStatus("l1")
+        l2.displayLightStatus("l2")
+    }
+}
+
+class Lampu{
+    private var isOn: Boolean = false
+
+    fun turnOn(){
+        isOn = true
+    }
+
+    fun turnOff(){
+        isOn = false
+    }
+
+    fun displayLightStatus(lamp: String){
+        if(isOn == true)
+            println("$lamp lampu is on.")
+        else
+            println("$lamp lampu is off.")
+    }
+
+    fun setStatusLamp(status: Boolean){
+        isOn = status
     }
 }
